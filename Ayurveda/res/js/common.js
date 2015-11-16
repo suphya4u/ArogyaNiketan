bfc9669b98ed6b569ede8e765b88c8c24b5a9db2
suhas <suphya4u@gmail.com>
2015-11-15 22:26:31 -0800

    Update header css on scroll.

--- Ayurveda/res/js/common.js
+++ Ayurveda/res/js/common.js
@@ -1,4 +1,5 @@
 
+var scrollHeaderSize = 550;
 
 function initCommonSections(selectedMenuItemId) {
   $("#header").load("html/header.html", function() {
@@ -8,3 +9,20 @@ function initCommonSections(selectedMenuItemId) {
   $("#footer").load("html/footer.html");
   $("#sidebar").load("html/sidebar.html");
 }
+
+function updateHeaderCss(scrollFraction) {
+    $("#header").css("background","rgba(150, 150, 150, " + (1 - scrollFraction) + ")");
+    $("#header").css("height", (70 + (80 * scrollFraction)) + "px");
+    $("#title_top").css("font-size", (140 + (40 * scrollFraction)) + "%");
+    $("#title_top").css("padding",
+    	(10 + (20 * scrollFraction)) + "px 0 " + (10 + (5 * scrollFraction)) + "px 0");
+    $("#title_bottom").css("padding", (10 * scrollFraction) + "px 0 0px 0");
+}
+
+$(document).scroll(function() {
+	var scrollTop = $(this).scrollTop();
+    if (scrollTop > scrollHeaderSize) {
+    	scrollTop = scrollHeaderSize;
+    }
+    updateHeaderCss((scrollHeaderSize - scrollTop) / scrollHeaderSize);
+})
