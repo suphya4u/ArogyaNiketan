edbbcdc32d8d82eb69dfca48d526046177b08d4c
suhas <suphya4u@gmail.com>
2015-11-19 15:35:14 -0800

    More style changes to create brown theme.

--- Ayurveda/res/js/common.js
+++ Ayurveda/res/js/common.js
@@ -11,18 +11,20 @@ function initCommonSections(selectedMenuItemId) {
 }
 
 function updateHeaderCss(scrollFraction) {
-    $("#header").css("background","rgba(150, 150, 150, " + (1 - scrollFraction) + ")");
+    $("#header").css("background","rgba(77, 37, 0, " + (1 - scrollFraction) + ")");
     $("#header").css("height", (70 + (80 * scrollFraction)) + "px");
-    $("#title_top").css("font-size", (140 + (40 * scrollFraction)) + "%");
+    $("#title_top").css("font-size", (140 + (60 * scrollFraction)) + "%");
     $("#title_top").css("padding",
-    	(10 + (20 * scrollFraction)) + "px 0 " + (10 + (5 * scrollFraction)) + "px 0");
+        (7 + (23 * scrollFraction)) + "px 0 " + (7 + (8 * scrollFraction)) + "px 0");
     $("#title_bottom").css("padding", (10 * scrollFraction) + "px 0 0px 0");
 }
 
-$(document).scroll(function() {
+function updateHeader() {
 	var scrollTop = $(this).scrollTop();
     if (scrollTop > scrollHeaderSize) {
     	scrollTop = scrollHeaderSize;
     }
     updateHeaderCss((scrollHeaderSize - scrollTop) / scrollHeaderSize);
-})
+}
+
+$(document).scroll(updateHeader);
